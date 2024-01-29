e2dedcc2154f3c78bf9a9953990d901b92b62440
Aris Husanu <Aris.Husanu@allscripts.com>
2024-01-29 18:14:06 -0500

    add some plugins and stuff
    Delete :q
    
    add helpful settings and keymaps

--- init.lua
+++ init.lua
@@ -303,7 +303,20 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  { -- Fuzzy Finder (files, lsp, etc)
+ {
+    -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    -- main = 'ibl',
+    -- opts = {},
+ },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- Fuzzy Finder (files, lsp, etc)
+  {
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
     branch = '0.1.x',
